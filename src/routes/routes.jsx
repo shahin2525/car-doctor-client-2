ca438d7bd53ed9bd8790abd1d8cb7847c1223f1e
shahin2525 <shahin451999@gmail.com>
2025-01-01 11:06:42 -0800

    add home

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router";
+import Main from "../layouts/Main";
+import Home from "../pages/home/home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: "",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
