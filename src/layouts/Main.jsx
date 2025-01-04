bb3d2be57293eba2a75d4ff393f695d1c54ae80c
shahin2525 <shahin451999@gmail.com>
2025-01-03 16:57:57 -0800

    add navbar

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/footer/Footer";
+import Navbar from "../pages/shared/navbar/Navbar";
 
 const Main = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       {/* <Home></Home> */}
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,25 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Main from "../layouts/Main";
 import Home from "../pages/home/home/Home";
-import About from "../pages/about/About";
-// import Home from "../pages/home/home/Home";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Main></Main>,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home></Home>,
-//       },
-//       {
-//         path: "/about",
-//         element: <About></About>,
-//       },
-//     ],
-//   },
-// ]);
 const router = createBrowserRouter([
   {
     path: "/",
