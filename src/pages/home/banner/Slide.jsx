0d66a99838c4888fa5d0aa0a7c5033d8f3506a25
shahin2525 <shahin451999@gmail.com>
2025-01-03 20:13:13 -0800

    add color gradient

--- src/pages/home/banner/Slide.jsx
+++ src/pages/home/banner/Slide.jsx
@@ -3,9 +3,9 @@ const Slide = ({ slide, img, iSlide1, iSlide2 }) => {
   return (
     <div id={slide} className="carousel-item relative w-full">
       <img src={img} className="w-full rounded-lg" />
-      <div className="absolute bottom-0 left-5 right-5 flex -translate-y-1/2 transform">
-        <div className="space-y-7 w-1/3">
-          <h1 className="text-white font-bold text-4xl">
+      <div className="absolute bottom-0 left-5 right-5 flex h-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+        <div className="space-y-5 w-1/3 pl-10 text-white">
+          <h1 className=" font-bold text-6xl">
             Affordable Price For Car Services
           </h1>
           <p>
